de92c21d036459202e2979db8cde5e72b1e36614
notMe <ahmad.amin.ackerman@gmail.com>
2022-12-08 01:29:21 +0100

    Oh

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { dnsProxy } = require('./dnsProxy.js');
 
-const proxy = new dnsProxy('127.0.0.1', 53, 'blacklist.txt');
+const proxy = new dnsProxy('0.0.0.0', 53, 'blacklist.txt');
 proxy.start();
 proxy.startMServer();
